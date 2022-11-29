483bd2532e81e9a855989b5cdcc6721d2814ce37
habeebah04 <hsalaubello124@gmail.com>
2022-11-29 11:22:20 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let = openMenu = document.getElementById("open")
+function showMenu(){
+    openMenu.style.right = "0px";
+}
+
+let = closeMenu = document.getElementById("open")
+function hideMenu(){
+    closeMenu.style.right= "-400px";
+}
